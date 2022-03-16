8a64f89e69a8e1494dc61b5bf3da54dc6a964ded
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-03-16 14:45:11 -0300

    Add redirect to home page test

--- tests/unit/server/routes.test.js
+++ tests/unit/server/routes.test.js
@@ -1,7 +1,41 @@
-import {jest, expect, describe, test} from '@jest/globals'
+import { jest, expect, describe, test, beforeEach } from '@jest/globals'
+import config from '../../../server/config.js'
+const {
+    pages,
+    location
+} = config
 
-describe('#Routes', ()=>{
-    test('my test', ()=>{
-        expect(true).toBeTruthy()
+import { handler } from '../../../server/routes'
+import TestUtil from '../_util/testUtil.js'
+
+describe('#Routes - test site for api response', () => {
+    beforeEach(() => {
+        jest.resetAllMocks()
+        jest.clearAllMocks()
+    })
+
+    test('GET / - should redirect to home page', async () => {
+        const params = TestUtil.defaultHandleParams()
+        params.request.method = 'GET'
+        params.request.url = '/'
+    
+        await handler(...params.values())
+    
+        expect(params.response.writeHead).toBeCalledWith(
+          302, {
+            'Location': location.home
+          }
+        )
+        expect(params.response.end).toHaveBeenCalled()
+      })
+    test.todo(`GET /home - should response with ${pages.homeHTML} file stream`)
+    test.todo(`GET /controller - should response with ${pages.controllerHTML} file stream`)
+    test.todo(`GET /file.ext - should response with file stream`)
+    test.todo(`GET /unknown - given an inexistent route it should response with 404`)
+
+
+    describe('Exceptions', () => {
+        test.todo('given inexistent file it should respond with 404')
+        test.todo('given inexistent file it should respond with 500')
     })
 })
