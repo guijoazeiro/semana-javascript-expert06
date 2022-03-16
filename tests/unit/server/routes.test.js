fc1e303ed2b39c9f42922146b6d504a249682973
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-03-16 15:07:02 -0300

    Add get controller  test

--- tests/unit/server/routes.test.js
+++ tests/unit/server/routes.test.js
@@ -1,64 +1,92 @@
-import { jest, expect, describe, test, beforeEach } from '@jest/globals'
+import {
+  jest,
+  expect,
+  describe,
+  test,
+  beforeEach
+} from '@jest/globals'
 import config from '../../../server/config.js'
-import { Controller } from '../../../server/controller.js'
-const {
-    pages,
-    location
-} = config
-
-import { handler } from '../../../server/routes'
+import {
+  Controller
+} from '../../../server/controller.js'
+import {
+  handler
+} from '../../../server/routes.js'
 import TestUtil from '../_util/testUtil.js'
 
+const {
+  pages,
+  location,
+  constants: {
+    CONTENT_TYPE
+  }
+} = config
+
 describe('#Routes - test site for api response', () => {
-    beforeEach(() => {
-        jest.resetAllMocks()
-        jest.clearAllMocks()
+  beforeEach(() => {
+    jest.restoreAllMocks()
+    jest.clearAllMocks()
+  })
+
+  test('GET / - should redirect to home page', async () => {
+    const params = TestUtil.defaultHandleParams()
+    params.request.method = 'GET'
+    params.request.url = '/'
+
+    await handler(...params.values())
+
+    expect(params.response.writeHead).toBeCalledWith(
+      302, {
+        'Location': location.home
+      }
+    )
+    expect(params.response.end).toHaveBeenCalled()
+  })
+  test(`GET /home - should response with ${pages.homeHTML} file stream`, async () => {
+    const params = TestUtil.defaultHandleParams()
+    params.request.method = 'GET'
+    params.request.url = '/home'
+    const mockFileStream = TestUtil.generateReadableStream(['data'])
+
+    jest.spyOn(
+      Controller.prototype,
+      Controller.prototype.getFileStream.name,
+    ).mockResolvedValue({
+      stream: mockFileStream,
     })
 
-    test('GET / - should redirect to home page', async () => {
-        const params = TestUtil.defaultHandleParams()
-        params.request.method = 'GET'
-        params.request.url = '/'
+    jest.spyOn(
+      mockFileStream,
+      "pipe"
+    ).mockReturnValue()
 
-        await handler(...params.values())
+    await handler(...params.values())
 
-        expect(params.response.writeHead).toBeCalledWith(
-            302, {
-            'Location': location.home
-        }
-        )
-        expect(params.response.end).toHaveBeenCalled()
+    expect(Controller.prototype.getFileStream).toBeCalledWith(pages.homeHTML)
+    expect(mockFileStream.pipe).toHaveBeenCalledWith(params.response)
+  })
+  test(`GET /controller - should response with ${pages.controllerHTML} file stream`, async () => {
+    const params = TestUtil.defaultHandleParams()
+    params.request.method = 'GET'
+    params.request.url = '/controller'
+    const mockFileStream = TestUtil.generateReadableStream(['data'])
+
+    jest.spyOn(
+      Controller.prototype,
+      Controller.prototype.getFileStream.name,
+    ).mockResolvedValue({
+      stream: mockFileStream,
     })
-    test(`GET /home - should response with ${pages.homeHTML} file stream`, async () => {
-        const params = TestUtil.defaultHandleParams()
-        params.request.method = 'GET'
-        params.request.url = '/home'
-        const mockFileStream = TestUtil.generateReadableStream(['data'])
 
-        jest.spyOn(
-            Controller.prototype,
-            Controller.prototype.getFileStream.name,
-        ).mockResolvedValue({
-            stream: mockFileStream,
-        })
+    jest.spyOn(
+      mockFileStream,
+      "pipe"
+    ).mockReturnValue()
 
-        jest.spyOn(
-            mockFileStream,
-            "pipe"
-        ).mockReturnValue()
+    await handler(...params.values())
 
-        await handler(...params.values())
-
-        expect(Controller.prototype.getFileStream).toBeCalledWith(pages.homeHTML)
-        expect(mockFileStream.pipe).toHaveBeenCalledWith(params.response)
-    })
-    test.todo(`GET /controller - should response with ${pages.controllerHTML} file stream`)
-    test.todo(`GET /file.ext - should response with file stream`)
-    test.todo(`GET /unknown - given an inexistent route it should response with 404`)
-
-
-    describe('Exceptions', () => {
-        test.todo('given inexistent file it should respond with 404')
-        test.todo('given inexistent file it should respond with 500')
-    })
+    expect(Controller.prototype.getFileStream).toBeCalledWith(pages.controllerHTML)
+    expect(mockFileStream.pipe).toHaveBeenCalledWith(params.response)
+  })
+  
 })
