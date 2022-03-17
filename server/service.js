9cee3877b0304542113aca59dae5594678e1a7dd
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-03-17 17:13:26 -0300

    Add getBitrate

--- server/service.js
+++ server/service.js
@@ -1,34 +1,75 @@
 import fs from 'fs'
 import config from './config.js'
-import {randomUUID} from 'crypto'
+import { randomUUID } from 'crypto'
 import { join, extname } from 'path'
 import fsPromises from 'fs/promises'
-import {PassThrough} from 'stream'
+import { PassThrough } from 'stream'
+import { } from 'throttle'
+import childProcess from 'child_process'
+import { logger } from './util.js'
 
 const {
     dir: {
         publicDirectory
+    },
+    constants: {
+        fallbckBitrate
     }
 } = config
 
 export class Service {
-    constructor(){
+    constructor() {
         this.clientStreams = new Map()
     }
 
-    createClientStream(){
+    createClientStream() {
         const id = randomUUID()
         const clientStreams = new PassThrough()
         this.clientStreams.set(id, clientStreams)
 
-        return{
+        return {
             id,
             clientStreams
         }
     }
-    removeClientStream(id){
+    removeClientStream(id) {
         this.clientStreams.delete(id)
     }
+
+    _executeSouxCommand(args) {
+        return childProcess.spawn('sox', args)
+    }
+
+    async getBitRage(song) {
+        try {
+            const args = [
+                '--i', //info
+                '--B',//bitrate
+                song
+            ]
+            const {
+                stderr,
+                stdout,
+                stdin } = this._executeSouxCommand(args)
+
+            await Promise.all([
+                once(stderr, 'readable'),
+                once(stdout, 'readable'),
+            ])
+
+            const [success, error] = [stdout, stderr].map(stream => stream.read())
+            if (error) return await Promise.reject(error)
+            return success
+                .toString()
+                .trim()
+                .replace(/k/, '000')
+
+        } catch (error) {
+            logger.error(`error no bitrate ${error} `)
+            return fallbckBitrate
+        }
+    }
+
     createFileStream(filename) {
         return fs.createReadStream(filename)
     }
