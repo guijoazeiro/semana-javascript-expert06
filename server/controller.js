174798777a541f2842467ba8be3d2a08edb48665
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-03-17 16:51:41 -0300

    Add createClientStream and remove

--- server/controller.js
+++ server/controller.js
@@ -1,11 +1,26 @@
 import { Service } from "./service.js";
 
 export class Controller {
-    constructor (){
+    constructor() {
         this.service = new Service()
     }
 
-    async getFileStream(filename){
+    async getFileStream(filename) {
         return this.service.getFileStrem(filename)
     }
+
+    createClientStream() {
+        const { id, clientStreams } = this.service.createClientStream()
+
+        const onClose = () => {
+            logger.info(`Closing connection of ${id}`)
+            this.service.removeClientStream(id)
+        }
+        return {
+            stream: clientStreams,
+            onClose
+
+        }
+    }
+
 }
